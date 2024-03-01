f2efad38b568a31457aa13f1214b0ad613004b41
jikingg <jaskin233@gmail.com>
2024-02-29 22:03:21 -0500

    fixing errors

--- mosaicmusic/webclient/views.py
+++ mosaicmusic/webclient/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.template import loader
 
 def home(request):
-  template = loader.get_template('/templates/index.html')
+  template = loader.get_template('index.html')
   return HttpResponse(template.render())
 
