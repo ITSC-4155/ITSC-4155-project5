4e10172176ac6249add9e2ed4ca83c620ddd0e37
jikingg <jaskin233@gmail.com>
2024-04-26 20:29:31 -0400

    changes

--- mosaicmusic/app/routers/search_routers.py
+++ mosaicmusic/app/routers/search_routers.py
@@ -11,15 +11,13 @@ search_blueprint = Blueprint('search', __name__, url_prefix='/search')
 def search():
     query = request.args.get('query', '')
 
-    if results is None:
-        flash('Please enter a song to search.')
-        return render_template('search.html', query=query, results=[])
+    results = deezer_client.search(query) 
     if not results:
         flash('No results found. Please check for typos or try different search terms.', 'info')
         return render_template('search.html', query=query, results=[])
     
 
-    results = deezer_client.search(query) 
+
 
     
     return render_template('search.html', query=query, results=results)
